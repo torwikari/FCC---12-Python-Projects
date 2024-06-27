76956f12aa40a09dd1fb007bf2e554a131d2c6fd
Torwikari <torwikari@gmail.com>
2024-06-27 13:02:55 +0200

    Guessing game

---
+++ rn.py
@@ -0,0 +1,34 @@
+import random 
+
+
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input("Guess the number: "))
+        if guess > random_number:
+            print("Your number is too big!")
+        elif guess < random_number:
+            print("Your number is too small!")
+        
+    print(f"Congratulations, you've guessed the number {random_number}! :)")
+    
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low 
+        feedback = input(f' is {guess} too high (H), too low (L), or correct (C)?')
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+            
+    print(f'Yay! the computer guessed your number {guess}, correctly!')
+    
+
+computer_guess(1000)
