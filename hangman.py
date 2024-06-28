51b7e546f1ce57034547ba8954028e26305d16d6
Torwikari <torwikari@gmail.com>
2024-06-28 19:55:30 +0200

    Hangman first draft

--- hangman.py
+++ hangman.py
@@ -4,14 +4,34 @@
 import random
 import ast
 
-def main_word():
+def word():
 
-    with open ('words.txt', 'r') as file:
-        content = file.read()
-    words = ast.literal_eval(content)
-    random_word = random.choice(words)
-    print(random_word)
+    with open ('words.txt', 'r') as file:       # Open the file without the need to close it. 
+        content = file.read()                   # Read the file. 
+    words = ast.literal_eval(content)           # Parse the data into the list with AST.
+    random_word = random.choice(words)          # Pick a random word. 
+    letters = len(random_word)                    # Count the number of letters of the word to know the users lives.
+    print(random_word, letters)
+    
+    # Welcome message, requires an input. Includes number of lives / letters.
+    user_guess = input(f"Your word has {letters} letters. You have the same amount of lives. Let's start.\nInput a letter: ")
+    
+    # Check if the input is empty and if it's a single letter.
+    while True:
+        if len(user_guess) != 1 or not user_guess.isAlpha() or user_guess == '':
+            print("Please input a single letter")
+    # Check if the random_word contain the user's input.
+        lives = letters
+        for x in random_word:
+            if lives == 0:              ## Check if the user is out of lives.
+                print("You're out of lives. Game end.")
+            elif x == user_guess:
+                print("Great, You've guessed the letter!")
+            elif x != user_guess:
+                lives -= 1
+                print(f"Try again! You have {lives} left! :)")
+    
+    # Add letter and repeat.
 
-
-main_word()
+word()
 
