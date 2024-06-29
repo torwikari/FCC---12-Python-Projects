13ccfd5518a797fb3c9f46de5f611de25aed7676
Torwikari <torwikari@gmail.com>
2024-06-29 13:13:00 +0200

    Changed 0-8 to 1-9 to have better UI

--- 12_pyt_proj/TicTacToe/game.py
+++ 12_pyt_proj/TicTacToe/game.py
@@ -17,7 +17,7 @@ class TicTacToe():
             
     @staticmethod
     def print_board_nums():
-        number_board = [[str(i) for i in range(j*3, (j+1)*3)] for j in range(3)] 
+        number_board = [[str(i + 1) for i in range(j*3, (j+1)*3)] for j in range(3)] 
         for row in number_board:
             print('| ' + ' | '.join(row) + ' |')
             
@@ -79,7 +79,7 @@ def play(game, x_player, o_player, print_game=True):
             if game.make_move(square, letter):
                 
                 if print_game:
-                    print(letter + f' makes a move to square {square}')
+                    print(letter + f' makes a move to square {square + 1}')
                     game.print_board()
                     print('') # just empty line.
                     
